c3eb7ee9e65801e6ab7348b32cd15a85a0de664e
lyn0719 <usolnim@naver.com>
2025-03-05 10:43:56 +0900

    Add 120810_나머지 구하기

---
+++ level-0/120810_나머지 구하기.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120810 - 나머지 구하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120810
+
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
