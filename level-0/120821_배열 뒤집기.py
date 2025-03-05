ed7d8f95856cada7b2c673fb0c647eda52367917
lyn0719 <usolnim@naver.com>
2025-03-05 11:54:52 +0900

    Add 120821_배열 뒤집기

---
+++ level-0/120821_배열 뒤집기.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120821 - 배열 뒤집기기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+def solution(num_list):
+    answer = num_list[::-1]
+    return answer
