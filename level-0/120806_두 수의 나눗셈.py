e5d26182a85b38eb3c919ca2a52952393c19ad7f
lyn0719 <usolnim@naver.com>
2025-03-05 19:27:06 +0900

    Add 120806_두 수의 나눗셈

---
+++ level-0/120806_두 수의 나눗셈.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120806 - 두 수의 나눗셈
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+def solution(num1, num2):
+    answer = int((num1 / num2)*1000)
+    return answer
