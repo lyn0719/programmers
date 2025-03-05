48455cace48a881f5ccf14e992da6877556bf536
lyn0719 <usolnim@naver.com>
2025-03-05 11:56:43 +0900

    Add 120898_편지

---
+++ level-0/120898_편지.py
@@ -0,0 +1,8 @@
+# 문제: 프로그래머스 120898 - 편지
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+
+def solution(message):
+    answer = len(message)
+    answer *= 2
+    return answer
