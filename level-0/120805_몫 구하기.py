35e1c4848364ddb6c67594ac8f54efa8f662b277
lyn0719 <usolnim@naver.com>
2025-03-05 10:31:25 +0900

    Add 120805_몫 구하기

---
+++ level-0/120805_몫 구하기.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120805 - 몫 구하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
