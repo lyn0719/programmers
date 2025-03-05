a567d667aa19a514256f61e2ca3febb7973dfc54
lyn0719 <usolnim@naver.com>
2025-03-05 19:23:23 +0900

    Add 120802_두 수의 합

---
+++ level-0/120802_두 수의 합.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120802 - 두 수의 합
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
