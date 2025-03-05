34a1a67ca28747223ded5a703ae5401e32ac08f6
lyn0719 <usolnim@naver.com>
2025-03-05 10:50:20 +0900

    Add 120804_두 수의 곱

---
+++ level-0/120804_두 수의 곱.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120804 - 두 수의 곱
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
