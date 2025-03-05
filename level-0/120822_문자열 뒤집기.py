0631c929bd759ba2bda744293b60112a1ab22f5c
lyn0719 <usolnim@naver.com>
2025-03-05 19:24:40 +0900

    Add 120822_문자열 뒤집기

---
+++ level-0/120822_문자열 뒤집기.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 120822 - 문자열 뒤집기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120822
+
+def solution(my_string):
+    answer = my_string[::-1]
+    return answer
