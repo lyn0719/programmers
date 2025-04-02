352017db230ceb7f352eff166442228a762aa47c
lyn0719 <usolnim@naver.com>
2025-04-02 09:23:19 +0900

    Add 12925_문자열을 정수로 바꾸기

---
+++ level-1/12925_문자열을 정수로 바꾸기.py
@@ -0,0 +1,8 @@
+# 문제: 프로그래머스 12925 - 문자열을 정수로 바꾸기
+# 난이도: Level 1
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+def solution(s):
+    answer = 0
+    answer = int(s)
+    return answer
