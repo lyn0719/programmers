6cf6bf4da5dc00309bbe4af76e99511972e44a35
lyn0719 <usolnim@naver.com>
2025-05-04 00:25:06 +0900

    Add 181946_문자열 붙여서 출력하기

---
+++ level-0/181946_문자열 붙여서 출력하기.py
@@ -0,0 +1,7 @@
+# 문제: 프로그래머스 181946 - 문자열 붙여서 출력하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+
+str1, str2 = input().strip().split(' ')
+
+print(f"{str1}{str2}")
