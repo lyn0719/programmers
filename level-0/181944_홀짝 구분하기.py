f6f96f3271f3b1a6e3d9a221cb61825dbc7aea4d
lyn0719 <usolnim@naver.com>
2025-05-04 00:27:22 +0900

    Add 181944_홀짝 구분하기

---
+++ level-0/181944_홀짝 구분하기.py
@@ -0,0 +1,10 @@
+# 문제: 프로그래머스 181944 - 홀짝 구분하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181944
+
+a = int(input())
+
+if a % 2 == 0:
+    print(f"{a} is even")
+else:
+    print(f"{a} is odd")
