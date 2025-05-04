f81e1183fa3261ff61aa0452071ef42cc2c62024
lyn0719 <usolnim@naver.com>
2025-05-05 00:00:24 +0900

    Add 181941_문자 리스트를 문자열로 변환하기

---
+++ level-0/181941_문자 리스트를 문자열로 변환하기.py
@@ -0,0 +1,9 @@
+# 문제: 프로그래머스 181941 - 문자 리스트를 문자열로 변환하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+def solution(arr):
+    answer = ''
+    
+    answer = ''.join(arr)
+    return answer
