7af3e32297f13e7e0df6758573d6184047a9024a
lyn0719 <usolnim@naver.com>
2025-05-07 00:03:35 +0900

    Add 181940_문자열 곱하기

---
+++ level-0/181940_문자열 곱하기.py
@@ -0,0 +1,9 @@
+# 문제: 프로그래머스 181940 - 문자열 곱하기
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181940
+
+def solution(my_string, k):
+    answer = ''
+    
+    answer = my_string * k
+    return answer
