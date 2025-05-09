786e7f7a8884033cd9c91a113bf6f1aa4b3aa21c
lyn0719 <usolnim@naver.com>
2025-05-09 16:06:27 +0900

    Add 181869_공백으로 구분하기 1

---
+++ level-0/181869_공백으로 구분하기 1.py
@@ -0,0 +1,9 @@
+# 문제: 프로그래머스 181869 - 공백으로 구분하기 1
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181869
+
+def solution(my_string):
+    answer = []
+    
+    answer = my_string.split()
+    return answer
