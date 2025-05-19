9bb87a375477711b0d97ca218d24e53474d2994a
lyn0719 <usolnim@naver.com>
2025-05-19 23:52:47 +0900

    Add 181892_n 번째 원소부터

---
+++ level-0/181892_n 번째 원소부터.py
@@ -0,0 +1,10 @@
+# 문제: 프로그래머스 181892 - n 번째 원소부터
+# 난이도: Level 0
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181892
+
+def solution(num_list, n):
+    answer = []
+    
+    answer = num_list[n-1:]
+        
+    return answer
